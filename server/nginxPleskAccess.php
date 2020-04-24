96c7833ab62ca67accdecefd68c92eca3cccc9de
Rik Zwarthoff <zwarthoffr@gmail.com>
2020-04-24 17:17:42 +0200

    Fix proxy netdata.

--- server/nginxPleskAccess.php
+++ server/nginxPleskAccess.php
@@ -54,7 +54,7 @@ server {
         proxy_redirect http://$host:19999 $scheme://$host/netdata;
         proxy_set_header  X-Real-IP  $remote_addr;
         proxy_set_header        Host            $host;
-        proxy_set_header        X-Forwarded-For $proxy_add_x_forwa$
+        proxy_set_header  X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header        X-Forwarded-Proto $scheme;
         proxy_buffering off;
         access_log off;
